4cc9d9d8035169a628d2dd850ab9fc1683147135
arborinlove <sb4893@naver.com>
2023-07-12 17:17:15 +0900

    수정

---
+++ index.js
@@ -0,0 +1,42 @@
+$('.event').each(function () {
+  $(this).on('wheel', function (evt) {
+    evt.preventDefault();
+
+    let delta = evt.originalEvent.deltaY;
+    let currentTop = null;
+    const nextBox = this.nextElementSibling;
+    const prevBox = this.previousElementSibling;
+
+    if (delta > 0) {
+      if (nextBox) {
+        currentTop = nextBox.offsetTop;
+      } else {
+        currentTop = this.offsetTop;
+      }
+    } else {
+      if (prevBox) {
+        currentTop = prevBox.offsetTop;
+      } else {
+        return;
+      }
+    }
+
+    scrollTo({
+      top: currentTop,
+      behavior: 'smooth',
+    });
+  });
+});
+
+// swiper
+new Swiper('.mySwiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  autoplay: {
+    delay: 3000,
+  },
+});
