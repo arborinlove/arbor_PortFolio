b92b57d1e894882f797830aeef04c1efd41b2f82
arborinlove <127369586+arborinlove@users.noreply.github.com>
2023-07-14 17:36:13 +0900

    수정

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
-$('.event').each(function () {
-  $(this).on('wheel', function (evt) {
-    evt.preventDefault();
-
-    let delta = evt.originalEvent.deltaY;
-    let currentTop = null;
-    const nextBox = this.nextElementSibling;
-    const prevBox = this.previousElementSibling;
-
-    if (delta > 0) {
-      if (nextBox) {
-        currentTop = nextBox.offsetTop;
-      } else {
-        currentTop = this.offsetTop;
-      }
-    } else {
-      if (prevBox) {
-        currentTop = prevBox.offsetTop;
-      } else {
-        return;
-      }
-    }
-
-    scrollTo({
-      top: currentTop,
-      behavior: 'smooth',
-    });
-  });
+/* top 클릭 시 위로 부드럽게 스크롤 */
+$('a[href="#"]').on('click', (evt) => {
+  evt.preventDefault();
 });
 
-// swiper
-new Swiper('.mySwiper', {
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  autoplay: {
-    delay: 3000,
-  },
+$('.btn').on('click', () => {
+  scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 });
 
