b1a4662dd6302c3cfbce88a9b55d202469cea85e
arborinlove <127369586+arborinlove@users.noreply.github.com>
2023-07-15 01:30:34 +0900

    수정

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-/* top 클릭 시 위로 부드럽게 스크롤 */
-$('a[href="#"]').on('click', (evt) => {
-  evt.preventDefault();
-});
-
-$('.btn').on('click', () => {
+$('.btn').on('click', (e) => {
+  e.preventDefault();
   scrollTo({
-    top: 0,
-    behavior: 'smooth',
+      top: 0,
+      behavior: "smooth"
   });
 });
 
+$(window).on('scroll', () => {
+  let scrollHt = $(document).scrollTop();
+});
