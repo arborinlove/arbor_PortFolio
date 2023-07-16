4745e7762c40bb83674e1368270905589e45c497
arborinlove <127369586+arborinlove@users.noreply.github.com>
2023-07-16 21:40:44 +0900

    수정

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ $('.btn').on('click', () => {
 const swiper = new Swiper('.mySwiper', {
   loop: true,
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
